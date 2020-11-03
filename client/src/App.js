3bc188c8ae31fae4b18af9f56587852da5f325cd
BrandonCay <brandoncay525@gmail.com>
2020-11-03 18:40:49 -0500

    again

---
+++ client/src/App.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import './App.css';
+
+const textSize="3";
+const barSize="9";
+const lgBarSize="10";
+const lgTextSize="2";
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      shortUrl:"",
+      originalUrl:""
+    }
+  }
+  
+  handleKeyPress(e){
+    if(e.charCode===13){
+      this.handleSubmit(e);
+    }
+  }
+  handleChange(e){
+    const name=e.target.name;
+    this.setState({
+      [name]:e.target.value //computed property name / partial state replacement syntax
+    });
+  }
+
+  handleSubmit(e){
+    this.setState({shortUrl:"Loading..."});
+    const oUrl={oUrl:this.state.originalUrl};
+    fetch('http://localhost:4000/api/shortUrl/new',{
+      method:'POST',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(oUrl)
+    }).then((res)=>{
+      return res.json();
+    }).then((data)=>{
+        if('error' in data){
+          this.setState({shortUrl:`${data.error}`});
+        }else{
+          this.setState({shortUrl:`http://localhost:4000/api/shorturl/new/${data.short_url}`});
+        }
+    }).catch((err)=>{return console.log(err)});
+
+  }
+  render(){
+    return (
+        <div className="App">
+          <Container fluid={true}>
+            <Row id="pRow" className="justify-content-center align-items-center">
+              <Col id="pCol" md="8" sm="8" xs="10" >
+
+                <Row>
+                  <Col sm="12" xs="12">
+                  <Form.Group > {/**  */}
+                  <Form.Row className="justify-content-end align-items-center">
+                    <Col md="12" sm="12" xs="12" id="title">
+                      <h1>
+                        Url-Shortener
+                      </h1>
+                    </Col>
+                  </Form.Row>
+                  </Form.Group>
+                  </Col>
+                </Row>
+
+
+                <Row id="row2">
+                  <Col sm="12">
+                  <Form.Group > {/**  */}
+                    <Form.Label srOnly>Enter URL to receive a shortened version of it</Form.Label>
+                    <Form.Row className="align-items-center">
+                    <Col lg={lgTextSize} md={textSize} sm={textSize} xs={textSize}>
+                      http://www.
+                    </Col>
+                    <Col lg={lgBarSize} md={barSize} sm={barSize} xs={barSize}>
+                      <Form.Control onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} placeholder="Enter Url you want to convert. Exclude the http(s)://www" name="originalUrl" />
+                    </Col>
+                    </Form.Row>
+                  </Form.Group>
+                  
+                  <Form.Group>
+                  <Form.Row className="justify-content-end">
+                  <Col  lg={lgBarSize} md={barSize} sm={barSize} xs={barSize}>
+                    <Button onClick={this.handleSubmit.bind(this)} variant="primary" id="button">Submit</Button>
+                    </Col>
+                    
+                  </Form.Row>
+                  </Form.Group>
+                
+                  <Form.Group id="btmGrp">
+                  <Form.Row  className="align-items-center">
+                    <Col lg={lgTextSize} md={textSize} sm={textSize} xs={textSize}>
+                      New Link:
+                    </Col>
+                    <Col lg={lgBarSize} md={barSize} sm={barSize} xs={barSize}>
+                    <Form.Control type="text"  value={`${this.state.shortUrl}`} readOnly/>
+                    </Col>
+                  </Form.Row>
+                  </Form.Group>
+
+                  </Col>
+                </Row>
+
+              </Col>
+            </Row>          
+          </Container>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,116 @@
+const express=require("express");
+const app=express();
+const dns=require("dns");
+const mongoose=require("mongoose");
+const cors=require("cors");
+const bodyParser=require("body-parser");
+const path=require("path");
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+/******
+ * Problems:
+ * dns.lookup only verfies simple links i.e. google.com but not google.com/search?adasdasdasd
+ * Title is misleading. URLs are not actually shortened but given an index. The user still needs to use the larger url
+ * nodemon not in -dev mode
+ */
+const mongo_uri=process.env.MONGDB_URI || 'mongodb://localhost/urlShortner';
+mongoose.connect(mongo_uri,{useNewUrlParser:true,useUnifiedTopology:true})
+
+const Schema=mongoose.Schema;
+const urlSchema=new Schema({
+    original_url:String,
+    short_url:String
+})
+const Url=mongoose.model('urls',urlSchema);
+app.post('/api/shorturl/new', (req,res)=>{ //oUrl=original url
+    console.log("body:",req.body);
+    const {oUrl} = req.body;
+    console.log(oUrl);
+
+    if(oUrl===undefined){
+        return res.json({error:"undefined", err});
+    }else if(oUrl===""){
+        return res.json({error:"No Input"});
+    }
+    
+    dns.lookup(oUrl,(err, address, family)=>{
+        if(err){
+            console.log(err);
+           return res.json({error:"invalid URL", details:err});
+           
+
+        }
+        else{
+            Url.findOne({original_url:oUrl},(err,result)=>{//checks if it exists
+                if(err){
+                    console.log('err',err);
+                    return res.json({error:err})
+
+                }else{
+
+                    if(result!==null){//if it does, then return the stored value   
+                        console.log(result);                
+                        res.json({original_url:oUrl, short_url:result.short_url});
+
+                    }else{//if it doesn't, create, save then return value. 
+                        Url.estimatedDocumentCount((err,count)=>{
+                            if(err){
+                                console.log('err',err);
+                                res.json({error:err});
+                                return;
+
+                            }
+                            else{
+                                const urlObject={original_url:oUrl,short_url:count};
+                                const urlDoc=new Url(urlObject);//short url using document count
+                                urlDoc.save((err,data)=>{
+                                    if(err){
+                                        return console.log("error: ",err);
+                                        
+                                    }else{
+                                        return res.json(urlObject);
+                                    }
+                                }) 
+                                
+
+                            }
+                        })
+                    }
+                }
+            })
+                
+
+        }
+    })
+    
+})
+
+app.get('/api/shorturl/new/:newUrl', (req,res)=>{
+    const shortUrl=req.params.newUrl;
+    Url.findOne({short_url:shortUrl},(err,result)=>{
+        if(err){
+            res.json({error:err});
+        }else{
+            res.redirect(`http://${result.original_url}`);
+            //changes url
+        }
+    })
+
+
+})
+
+if(process.env.NODE_ENV === 'production'){ //for heroku
+    app.use(express.static('../client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve('__dirname','../client','build','index.html'));//possible error here
+    })
+}
+
+const PORT=process.env.PORT || 4000;
+
+
+app.listen(PORT, ()=>{console.log(`port: ${PORT}`)});
